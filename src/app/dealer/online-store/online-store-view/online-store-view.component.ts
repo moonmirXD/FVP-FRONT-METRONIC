7b6bbf33ec19a9d96447451f6ecfdf98af2e6dd9
Ryan <arquezaryan@yahoo.com>
2020-01-06 09:00:56 +0800

    debugging

--- src/app/dealer/online-store/online-store-view/online-store-view.component.ts
+++ src/app/dealer/online-store/online-store-view/online-store-view.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'kt-online-store-view',
-  templateUrl: './online-store-view.component.html',
-  styleUrls: ['./online-store-view.component.scss']
+	selector: "kt-online-store-view",
+	templateUrl: "./online-store-view.component.html",
+	styleUrls: ["./online-store-view.component.scss"]
 })
 export class OnlineStoreViewComponent implements OnInit {
+	constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
+	onEdit() {
+		this.router.navigate(["/personal-details/online-store/edit"]);
+	}
 }
